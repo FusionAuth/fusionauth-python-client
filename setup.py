940ec3fb4799f3ace793705f9f96b462cb824caf
Tyler Scott <tyler@inversoft.com>
2018-10-29 16:46:02 -0600

    Pypi stuff and some fixes

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fusionauth-client",
+    version="1.0.17",
+    author="Tyler Scott",
+    author_email="tyler@inversoft.com",
+    description="A client library for FusionAuth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FusionAuth/fusionauth-python-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries"
+    ]
+)
