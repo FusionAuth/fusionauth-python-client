0acb5a7735e9247d0b47cea4ae565bd4d524821f
Brian Pontarelli <brian@inversoft.com>
2019-06-19 13:38:07 -0600

    Updated version for fusionauth-python-client to 1.7.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fusionauth-client",
-    version="1.7.1",
+    version="1.7.2",
     author="Tyler Scott",
     author_email="tyler@inversoft.com",
     description="A client library for FusionAuth",
