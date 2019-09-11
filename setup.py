aed37a9cc90a8dd7660d928285e49e56da22bade
Daniel DeGroff <daniel@fusionauth.io>
2019-09-10 19:38:23 -0600

    Prep for 1.8.1-RC.1 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fusionauth-client",
-    version="1.8.0-RC.1",
+    version="1.8.1-RC.1",
     author="Tyler Scott",
     author_email="tyler@inversoft.com",
     description="A client library for FusionAuth",
