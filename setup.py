0e49826b6bdbf2d601681b665e9dab1471cfc58b
Daniel DeGroff <daniel@fusionauth.io>
2020-03-30 21:11:26 -0600

    Updated version for fusionauth-python-client to 1.15.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fusionauth-client",
-    version="1.15.6",
+    version="1.15.7",
     author="Tyler Scott",
     author_email="tyler@inversoft.com",
     description="A client library for FusionAuth",
