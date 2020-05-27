f96614fe42a9e34f1eb3f98d32ac20e548273c44
Daniel DeGroff <daniel@fusionauth.io>
2020-05-27 11:47:09 -0600

    squash merge apple-login to master

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fusionauth-client",
-    version="1.16.1",
+    version="1.17.0",
     author="Tyler Scott",
     author_email="tyler@inversoft.com",
     description="A client library for FusionAuth",
