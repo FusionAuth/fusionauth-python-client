4697ba58583ee1043cb6ebe0fde29ba14e99c482
Daniel DeGroff <daniel@fusionauth.io>
2020-12-22 11:16:51 -0700

    squash merge tx-protection into master

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fusionauth-client",
-    version="1.22.3",
+    version="1.22.4",
     author="Tyler Scott",
     author_email="dev@fusionauth.io",
     description="A client library for FusionAuth",
