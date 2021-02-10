bcdf88f25546f0e31b12b6a59a80bf04046bce0d
Daniel DeGroff <daniel@fusionauth.io>
2021-02-10 14:36:06 -0700

    Support deleting refresh tokens by Id.

--- src/main/python/fusionauth/fusionauth_client.py
+++ src/main/python/fusionauth/fusionauth_client.py
@@ -2480,8 +2480,30 @@ class FusionAuthClient:
 
     def revoke_refresh_token(self, token=None, user_id=None, application_id=None):
         """
-        Revokes a single refresh token, all tokens for a user or all tokens for an application. If you provide a user id
-        and an application id, this will delete all the refresh tokens for that user for that application.
+        Revokes refresh tokens.
+        
+        Usage examples:
+          - Delete a single refresh token, pass in only the token.
+              revokeRefreshToken(token)
+        
+          - Delete all refresh tokens for a user, pass in only the userId.
+              revokeRefreshToken(null, userId)
+        
+          - Delete all refresh tokens for a user for a specific application, pass in both the userId and the applicationId.
+              revokeRefreshToken(null, userId, applicationId)
+        
+          - Delete all refresh tokens for an application
+              revokeRefreshToken(null, null, applicationId)
+        
+        Note: <code>null</code> may be handled differently depending upon the programming language.
+        
+        See also: (method names may vary by language... but you'll figure it out)
+        
+         - revokeRefreshTokenById
+         - revokeRefreshTokenByToken
+         - revokeRefreshTokensByUserId
+         - revokeRefreshTokensByApplicationId
+         - revokeRefreshTokensByUserIdForApplication
 
         Attributes:
             token: (Optional) The refresh token to delete.
@@ -2490,6 +2512,68 @@ class FusionAuthClient:
         """
         return self.start().uri('/api/jwt/refresh') \
             .url_parameter('token', token) \
+            .url_parameter('userId', user_id) \
+            .url_parameter('applicationId', application_id) \
+            .delete() \
+            .go()
+
+    def revoke_refresh_token_by_id(self, token_id):
+        """
+        Revokes a single refresh token by the unique Id. The unique Id is not sensitive as it cannot be used to obtain another JWT.
+
+        Attributes:
+            token_id: The unique Id of the token to delete.
+        """
+        return self.start().uri('/api/jwt/refresh') \
+            .url_segment(token_id) \
+            .delete() \
+            .go()
+
+    def revoke_refresh_token_by_token(self, token):
+        """
+        Revokes a single refresh token by using the actual refresh token value. This refresh token value is sensitive, so  be careful with this API request.
+
+        Attributes:
+            token: The refresh token to delete.
+        """
+        return self.start().uri('/api/jwt/refresh') \
+            .url_parameter('token', token) \
+            .delete() \
+            .go()
+
+    def revoke_refresh_tokens_by_application_id(self, application_id):
+        """
+        Revoke all refresh tokens that belong to an application by applicationId.
+
+        Attributes:
+            application_id: The unique Id of the application that you want to delete all refresh tokens for.
+        """
+        return self.start().uri('/api/jwt/refresh') \
+            .url_parameter('applicationId', application_id) \
+            .delete() \
+            .go()
+
+    def revoke_refresh_tokens_by_user_id(self, user_id):
+        """
+        Revoke all refresh tokens that belong to a user by user Id.
+
+        Attributes:
+            user_id: The unique Id of the user that you want to delete all refresh tokens for.
+        """
+        return self.start().uri('/api/jwt/refresh') \
+            .url_parameter('userId', user_id) \
+            .delete() \
+            .go()
+
+    def revoke_refresh_tokens_by_user_id_for_application(self, user_id, application_id):
+        """
+        Revoke all refresh tokens that belong to a user by user Id for a specific application by applicationId.
+
+        Attributes:
+            user_id: The unique Id of the user that you want to delete all refresh tokens for.
+            application_id: The unique Id of the application that you want to delete refresh tokens for.
+        """
+        return self.start().uri('/api/jwt/refresh') \
             .url_parameter('userId', user_id) \
             .url_parameter('applicationId', application_id) \
             .delete() \
