ae9eb259392f7315d10946d7bf8be1d7f2982c1d
Daniel DeGroff <daniel@fusionauth.io>
2022-03-08 01:14:36 -0700

    Squash merge degroff/graal-fetch-baller to master

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fusionauth-client",
-    version="1.34.2",
+    version="1.99.9",
     author="Tyler Scott",
     author_email="dev@fusionauth.io",
     description="A client library for FusionAuth",
