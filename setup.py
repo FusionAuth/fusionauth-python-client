2b3113fcb29b95e6e19d2453b5910a23188e6e7c
David Charles <david.charles@fusionauth.io>
2024-04-04 11:33:10 -0600

    Updated version for fusionauth-python-client to 1.50.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fusionauth-client",
-    version="1.50.0-EAP.20240327",
+    version="1.50.0",
     author="Tyler Scott",
     author_email="dev@fusionauth.io",
     description="A client library for FusionAuth",
