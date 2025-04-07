6087583af76a870e8464dd1f586e414ac6e61481
Lyle Schemmerling <lshem16@gmail.com>
2025-04-07 14:01:07 -0600

    publish update

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+#  Copyright (c) 2025, FusionAuth, All Rights Reserved
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+#  either express or implied. See the License for the specific
+#  language governing permissions and limitations under the License.
+
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
@@ -9,6 +23,8 @@ setup(
     author="FusionAuth",
     author_email="dev@fusionauth.io",
     description="A client library for FusionAuth",
+    license="Apache-2.0",
+    license_files=("LICENSE",),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/FusionAuth/fusionauth-python-client",
